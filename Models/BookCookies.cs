df4a2859ca272f78d7892092bd0423bceca0f494
SamuelNguyen2121 <2032373@champlaincollege.qc.ca>
2023-11-23 21:28:48 -0500

    Fixed Edit Books, Tried to Fix Cookie

--- Models/BookCookies.cs
+++ Models/BookCookies.cs
@@ -22,23 +22,23 @@
 
         public void SetMyBookIds(List<Book> mybooks)
         {
-            List<int> ids = mybooks.Select(b => b.BookId).ToList();
-            string idsString = String.Join(Delimiter, ids);
-            CookieOptions options = new CookieOptions
-            {
-                Expires = DateTime.Now.AddDays(30)
-            };
-            RemoveMyBookIds();     // delete old cookie first
-            responseCookies.Append(BooksKey, idsString, options);
-        }
+			List<string> ids = mybooks.Select(b => b.BookId.ToString()).ToList();
+			string idsString = String.Join(Delimiter, ids);
+			CookieOptions options = new CookieOptions
+			{
+				Expires = DateTime.Now.AddDays(30)
+			};
+			RemoveMyBookIds(); // Delete old cookie first
+			responseCookies.Append(BooksKey, idsString, options);
+		}
 
-        public string[] GetMyBooksIds()
+        public int[] GetMyBooksIds()
         {
             string cookie = requestCookies[BooksKey] ?? String.Empty;
             if (string.IsNullOrEmpty(cookie))
-                return Array.Empty<string>();   // empty string array
+                return Array.Empty<int>();   
             else
-                return cookie.Split(Delimiter);
+                return cookie.Split(Delimiter).Select(int.Parse).ToArray();
         }
 
         public void RemoveMyBookIds()
